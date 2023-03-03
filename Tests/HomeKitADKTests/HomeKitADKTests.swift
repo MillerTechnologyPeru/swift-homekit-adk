5e8ba55f4e5be89643b81828ea631214314010ce
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-03-03 11:23:00 -0800

    Updated unit tests

--- Tests/HomeKitADKTests/HomeKitADKTests.swift
+++ Tests/HomeKitADKTests/HomeKitADKTests.swift
@@ -2,10 +2,9 @@ import XCTest
 @testable import HomeKitADK
 
 final class HomeKitADKTests: XCTestCase {
+     
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(HomeKitADK().text, "Hello, World!")
+        
+        //XCTAssertEqual(HomeKitADK().text, "Hello, World!")
     }
 }
